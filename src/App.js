88ed0102e7d95152217e566bde69a9f4c0cf0d0a
Lucas Rafael Barbosa <lucasbarbosa@franca.sp.gov.br>
2021-11-19 19:27:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import Tmdb from './Tmdb';
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=> {
+    const loadAll = async () => {
+      let list = await Tmdb.gethomeList();
+      setMovieList(list);
+    }
+    loadAll();
+  }, []);
+
+  return(
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            {item.title}
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
